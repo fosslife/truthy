5f4ae9afb94ac36a3d36245994780c918f9e797b
Sparkenstein <zetabytes.pp@gmail.com>
2024-06-14 19:54:45 +0530

    feat: export db manually

---
+++ src/components/Modals/ExportModal.tsx
@@ -0,0 +1,218 @@
+import { Button, Modal, Stack } from "@mantine/core";
+import { memo } from "react";
+import { useAppContext } from "../../contexts/App";
+import { confirm, save } from "@tauri-apps/api/dialog";
+import { writeTextFile, copyFile, BaseDirectory } from "@tauri-apps/api/fs";
+import { notifications } from "@mantine/notifications";
+
+type ExportModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+function ExportModalBase({ isOpen, onClose }: ExportModalProps) {
+  const { entries, db } = useAppContext();
+
+  const exportJson = async () => {
+    const confirmation = await confirm(
+      "This will save all your secrets in a plain-text JSON file. Are you sure you want to continue?",
+      {
+        type: "warning",
+        okLabel: "Yes, I am sure",
+        cancelLabel: "No, take me back",
+      }
+    );
+    if (!confirmation) return;
+    const path = await save({
+      title: "Export JSON",
+      defaultPath: "otps.json",
+      filters: [{ name: "JSON", extensions: ["json"] }],
+    });
+
+    if (path) {
+      // const finalPath = await join(path, "otps.json");
+      await writeTextFile(path, JSON.stringify(entries, null, 2));
+      notifications.show({
+        message: "Exported JSON",
+        color: "green",
+      });
+      onClose();
+    }
+  };
+
+  const exportAes = async () => {
+    const confirmation = await confirm(
+      "This will save all your secrets in an encrypted JSON file. Are you sure you want to continue?",
+      {
+        type: "warning",
+        okLabel: "Yes, I am sure",
+        cancelLabel: "No, take me back",
+      }
+    );
+    if (!confirmation) return;
+    const path = await save({
+      title: "Export JSON",
+      defaultPath: "otps.aes.json",
+      filters: [{ name: "JSON", extensions: ["json"] }],
+    });
+
+    if (path) {
+      const plainText = JSON.stringify(entries, null, 2);
+      const password = prompt("Enter password to encrypt the JSON file");
+
+      if (!password) {
+        notifications.show({
+          message: "Password is required",
+          color: "red",
+        });
+        return;
+      }
+
+      const encrypted = await encrypt(plainText, password);
+      await writeTextFile(path, encrypted);
+      notifications.show({
+        message: "Exported encrypted JSON",
+        color: "green",
+      });
+
+      onClose();
+    }
+  };
+
+  const exportKdbxXml = async () => {
+    const confirmation = await confirm(
+      "This will save all your secrets in a KDBX XML file. Are you sure you want to continue?",
+      {
+        type: "warning",
+        okLabel: "Yes, I am sure",
+        cancelLabel: "No, take me back",
+      }
+    );
+
+    if (!confirmation) return;
+
+    const path = await save({
+      title: "Export KDBX XML",
+      defaultPath: "otps.kdbx.xml",
+      filters: [{ name: "KDBX XML", extensions: ["xml"] }],
+    });
+
+    if (path) {
+      const content = await db?.saveXml();
+      if (content) {
+        await writeTextFile(path, content);
+        notifications.show({
+          message: "Exported KDBX XML",
+          color: "green",
+        });
+        onClose();
+      }
+    }
+  };
+
+  const exportKdbx = async () => {
+    const confirmation = await confirm(
+      "This will save all your secrets in a KDBX file. Are you sure you want to continue?",
+      {
+        type: "warning",
+        okLabel: "Yes, I am sure",
+        cancelLabel: "No, take me back",
+      }
+    );
+
+    if (!confirmation) return;
+
+    const path = await save({
+      title: "Export KDBX",
+      defaultPath: "otps.kdbx",
+      filters: [{ name: "KDBX", extensions: ["kdbx"] }],
+    });
+
+    if (path) {
+      await copyFile("vault.kdbx", path, {
+        dir: BaseDirectory.App,
+      });
+      notifications.show({
+        message: "Exported KDBX",
+        color: "green",
+      });
+      onClose();
+    }
+  };
+
+  return (
+    <Modal
+      opened={isOpen}
+      onClose={onClose}
+      title="Pick option to export"
+      keepMounted={false}
+    >
+      <Stack>
+        <Button onClick={exportJson}>Export plain text JSON</Button>
+
+        <Button onClick={exportAes}>Export Aes encrypted JSON</Button>
+
+        <Button onClick={exportKdbxXml}>Export KDBX XML</Button>
+
+        <Button onClick={exportKdbx}>Export encrypted KDBX</Button>
+      </Stack>
+    </Modal>
+  );
+}
+
+export const ExportModal = memo(ExportModalBase, (prev, next) => {
+  return prev.isOpen === next.isOpen;
+});
+
+async function encrypt(plaintext: string, password: string) {
+  const pwUtf8 = new TextEncoder().encode(password); // encode password as UTF-8
+  const pwHash = await crypto.subtle.digest("SHA-256", pwUtf8); // hash the password
+
+  const iv = crypto.getRandomValues(new Uint8Array(12)); // get 96-bit random iv
+  const ivStr = Array.from(iv)
+    .map((b) => String.fromCharCode(b))
+    .join(""); // iv as utf-8 string
+
+  const alg = { name: "AES-GCM", iv: iv }; // specify algorithm to use
+
+  const key = await crypto.subtle.importKey("raw", pwHash, alg, false, [
+    "encrypt",
+  ]); // generate key from pw
+
+  const ptUint8 = new TextEncoder().encode(plaintext); // encode plaintext as UTF-8
+  const ctBuffer = await crypto.subtle.encrypt(alg, key, ptUint8); // encrypt plaintext using key
+
+  const ctArray = Array.from(new Uint8Array(ctBuffer)); // ciphertext as byte array
+  const ctStr = ctArray.map((byte) => String.fromCharCode(byte)).join(""); // ciphertext as string
+
+  return btoa(ivStr + ctStr);
+}
+
+// Unused for now, but make a new modal that can decrypt.
+async function aesGcmDecrypt(ciphertext: string, password: string) {
+  const pwUtf8 = new TextEncoder().encode(password); // encode password as UTF-8
+  const pwHash = await crypto.subtle.digest("SHA-256", pwUtf8); // hash the password
+
+  const ivStr = atob(ciphertext).slice(0, 12); // decode base64 iv
+  const iv = new Uint8Array(Array.from(ivStr).map((ch) => ch.charCodeAt(0))); // iv as Uint8Array
+
+  const alg = { name: "AES-GCM", iv: iv }; // specify algorithm to use
+
+  const key = await crypto.subtle.importKey("raw", pwHash, alg, false, [
+    "decrypt",
+  ]); // generate key from pw
+
+  const ctStr = atob(ciphertext).slice(12); // decode base64 ciphertext
+  const ctUint8 = new Uint8Array(
+    Array.from(ctStr).map((ch) => ch.charCodeAt(0))
+  ); // ciphertext as Uint8Array
+  // note: why doesn't ctUint8 = new TextEncoder().encode(ctStr) work?
+
+  try {
+    const plainBuffer = await crypto.subtle.decrypt(alg, key, ctUint8); // decrypt ciphertext using key
+    const plaintext = new TextDecoder().decode(plainBuffer); // plaintext from ArrayBuffer
+    return plaintext; // return the plaintext
+  } catch (e) {
+    throw new Error("Decrypt failed");
+  }
+}
--- src/pages/Home/components/Sidebar.tsx
+++ src/pages/Home/components/Sidebar.tsx
@@ -1,12 +1,13 @@
 import {
   Button,
-  Checkbox,
   Divider,
   Drawer,
   Group,
-  Radio,
+  Select,
   Stack,
   Switch,
+  Text,
+  Title,
   rem,
   useMantineColorScheme,
   useMantineTheme,
@@ -15,6 +16,7 @@ import { useDisclosure } from "@mantine/hooks";
 import { IconMoonStars, IconSun } from "@tabler/icons-react";
 import { ImportModal } from "../../../components/Modals/ImportModal";
 import { useSettings, Settings } from "../../../contexts/Settings";
+import { ExportModal } from "../../../components/Modals/ExportModal";
 
 type Props = {
   drawerOpened: boolean;
@@ -25,6 +27,9 @@ export const Sidebar = ({ closeDrawer, drawerOpened }: Props) => {
   const { toggleColorScheme } = useMantineColorScheme();
 
   const [importOpen, { close: closeImport, open: openImport }] =
+    useDisclosure();
+
+  const [exportOpen, { close: closeExport, open: openExport }] =
     useDisclosure();
 
   const { settings, setSettings } = useSettings();
@@ -57,61 +62,97 @@ export const Sidebar = ({ closeDrawer, drawerOpened }: Props) => {
     <Drawer
       offset={8}
       radius={"md"}
-      //   size={"xs"}
       opened={drawerOpened}
       onClose={closeDrawer}
       title="Settings"
     >
       <Stack>
-        <Switch
-          size="md"
-          color="dark.4"
-          onLabel={moonIcon}
-          offLabel={sunIcon}
-          onChange={toggleColorScheme}
-          label="Theme"
-        />
-        <Divider />
-        <Switch
-          size="md"
-          onChange={() =>
-            saveAndMergeSettings({ blurMode: !settings?.blurMode })
-          }
-          checked={settings?.blurMode}
-          label="Blur mode"
-        />
-        <Divider />
-        <Radio.Group
-          label="Digits grouping"
-          value={settings.digitGrouping.toString()}
-          onChange={(e) => saveAndMergeSettings({ digitGrouping: e })}
-        >
-          <Group>
-            <Radio value={"2"} label="2" />
-            <Radio value={"3"} label="3" />
-          </Group>
-        </Radio.Group>
-        <Divider />
-        <Checkbox
-          checked={settings.minimizeOnCopy}
-          onChange={() =>
-            saveAndMergeSettings({ minimizeOnCopy: !settings.minimizeOnCopy })
-          }
-          label="Minimize on copy"
-        />
+        <Title c="brand" order={6}>
+          Appearance
+        </Title>
+        <Group px="lg" justify="space-between">
+          <Text>Colorscheme</Text>
+          <Switch
+            size="md"
+            color="dark.4"
+            onLabel={sunIcon}
+            offLabel={moonIcon}
+            onChange={toggleColorScheme}
+          />
+        </Group>
+        <Group px="lg" justify="space-between">
+          <Text>Blur digits</Text>
+          <Switch
+            size="md"
+            onChange={() =>
+              saveAndMergeSettings({ blurMode: !settings?.blurMode })
+            }
+            checked={settings?.blurMode}
+          />
+        </Group>
+        <Group px="lg" justify="space-between">
+          <Text>Digit grouping</Text>
+          <Select
+            w={"80"}
+            allowDeselect={false}
+            data={["2", "3"]}
+            value={settings.digitGrouping.toString()}
+            onChange={(e) => {
+              saveAndMergeSettings({ digitGrouping: e! });
+            }}
+          />
+        </Group>
 
+        <Group px="lg" justify="space-between">
+          <Text>View</Text>
+          <Select
+            w={"120"}
+            allowDeselect={false}
+            data={["compact", "card"]}
+            value={settings.view}
+            onChange={(e) => {
+              saveAndMergeSettings({ view: e as "compact" | "card" });
+            }}
+          />
+        </Group>
+
+        <Divider />
+        <Title c="brand" order={6}>
+          Behaviour
+        </Title>
+        <Group px="lg" justify="space-between">
+          <Text>Minimize on copy</Text>
+          <Switch
+            size="md"
+            onChange={() =>
+              saveAndMergeSettings({ minimizeOnCopy: !settings.minimizeOnCopy })
+            }
+            checked={settings.minimizeOnCopy}
+          />
+        </Group>
+        <Divider />
+
+        <Title c="brand" order={6}>
+          Import / Export
+        </Title>
         <Group>
           <Button onClick={openImport}>Import</Button>
+          <Button onClick={openExport}>Export</Button>
+        </Group>
+
+        <Divider />
+
+        <Title c="brand" order={6}>
+          Security
+        </Title>
+
+        <Group>
+          <Button onClick={() => {}}>Change master password</Button>
         </Group>
       </Stack>
       {/* planned settings:
-        1. Export/Import
-        2. Change master password
-        3. Backup
-        4. Spacing between numbers
-        5. Dark mode
-        6. Auto lock
-      
+        1. Export
+        2. Change master password      
       */}
 
       <ImportModal
@@ -119,6 +160,8 @@ export const Sidebar = ({ closeDrawer, drawerOpened }: Props) => {
         onClose={closeImport}
         closeDrawer={closeDrawer}
       />
+
+      <ExportModal isOpen={exportOpen} onClose={closeExport} />
     </Drawer>
   );
 };
