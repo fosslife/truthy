46e3c3c0ef190eca681ab1dec018786624515bb5
Sparkenstein <zetabytes.pp@gmail.com>
2024-06-15 21:40:47 +0530

    chore: lint

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,7 @@ import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {
-  MantineColorsTuple,
-  MantineProvider,
-  createTheme,
-} from "@mantine/core";
+
 import { Notifications } from "@mantine/notifications";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./contexts/App";
